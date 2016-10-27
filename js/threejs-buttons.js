77def5081fd57cd1565e9899122ecbf9fd190e18
Brian Peiris <brianpeiris@gmail.com>
2016-10-26 18:59:49 -0700

    Disable input for non-moderators. Finalize gear support

--- js/threejs-buttons.js
+++ js/threejs-buttons.js
@@ -1,58 +1,63 @@
 /* global scene */
-(function () {
 'use strict'
 
-if (!altspace.inClient) { return; }
+function addThreeJsButtons(user) {
+	if (!altspace.inClient) { return; }
 
-var zoomFactor = 1.25;
-var clickablesRoot = new THREE.Group();
-altspace.getEnclosure().then(function (enclosure) {
-	clickablesRoot.position.x = -enclosure.innerWidth / 2;
-	clickablesRoot.position.y = enclosure.innerHeight / 2;
-});
-scene.add(clickablesRoot);
+	if (!user.isModerator) { return; }
+	var isAndroid = /mobile/i.test(navigator.userAgent);
 
-var clickableGeo = new THREE.PlaneGeometry(1, 1, 1);
-var clickableMat = new THREE.MeshBasicMaterial();
-
-var clickables = [];
-
-function scaleAndPositionClickable(mesh, link) {
-	var outerWidth = link.outerWidth();
-	if (outerWidth) {
-		mesh.scale.x = Math.ceil(outerWidth) * zoomFactor;
-		mesh.scale.y = Math.ceil(link.outerHeight()) * zoomFactor;
-		mesh.visible = true;
-	}
-	else {
-		mesh.visible = false;
-	}
-
-	var linkOffset = link.offset();
-	mesh.position.x = linkOffset.left * zoomFactor + mesh.scale.x / 2;
-	mesh.position.y = -linkOffset.top * zoomFactor - mesh.scale.y / 2;
-	mesh.position.z = 100;
-}
-
-function reScaleAndPositionClickables() {
-	clickables.forEach(function (clickable) {
-		scaleAndPositionClickable(clickable.mesh, clickable.link);
+	var zoomFactor = isAndroid ? 1 : 1.25;
+	var clickablesRoot = new THREE.Group();
+	altspace.getEnclosure().then(function (enclosure) {
+		clickablesRoot.position.x = -enclosure.innerWidth / 2;
+		clickablesRoot.position.y = enclosure.innerHeight / 2;
 	});
-}
+	scene.add(clickablesRoot);
 
-$('[onclick], input').each(function () {
-	var link = $(this);
-	var mesh = new THREE.Mesh(clickableGeo, clickableMat);
-	clickablesRoot.add(mesh);
+	var clickableGeo = new THREE.PlaneGeometry(1, 1, 1);
+	var clickableMat = new THREE.MeshBasicMaterial({visible: false});
 
-	scaleAndPositionClickable(mesh, link);
-	clickables.push({mesh: mesh, link: link});
+	var clickables = [];
 
-	mesh.addEventListener('cursorup', function () {
-		link[0].onclick();
-		link.focus();
-		reScaleAndPositionClickables();
+	function scaleAndPositionClickable(mesh, el) {
+		var outerWidth = el.outerWidth();
+		if (el.is(':visible') && outerWidth) {
+			mesh.scale.x = Math.ceil(outerWidth) * zoomFactor;
+			mesh.scale.y = Math.ceil(el.outerHeight()) * zoomFactor;
+			mesh.visible = true;
+		}
+		else {
+			mesh.visible = false;
+		}
+
+		var elOffset = el.offset();
+		mesh.position.x = elOffset.left * zoomFactor + mesh.scale.x / 2;
+		mesh.position.y = -elOffset.top * zoomFactor - mesh.scale.y / 2;
+		mesh.position.z = 5;
+	}
+
+	function reScaleAndPositionClickables() {
+		clickables.forEach(function (clickable) {
+			scaleAndPositionClickable(clickable.mesh, clickable.el);
+		});
+	}
+
+	$('[onclick], input').each(function () {
+		var $el = $(this);
+		var mesh = new THREE.Mesh(clickableGeo, clickableMat);
+		clickablesRoot.add(mesh);
+
+		scaleAndPositionClickable(mesh, $el);
+		clickables.push({mesh: mesh, el: $el});
+
+		mesh.addEventListener('cursorup', function () {
+			var el = $el[0];
+			if (el.onclick) { el.onclick(); }
+			if (el.select) { el.select(); }
+			$el.focus();
+			reScaleAndPositionClickables();
+		});
 	});
-});
 
-}());
+}
