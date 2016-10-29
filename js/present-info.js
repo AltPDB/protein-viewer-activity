b890fc2063ca39d15f892e57d90d781bf804eb4e
tlskillman <tlskillman@gmail.com>
2016-10-28 21:27:51 -0700

    Add immune system graphic

--- js/present-info.js
+++ js/present-info.js
@@ -4,9 +4,9 @@ html2canvas(document.getElementById('2VAA-info')).then(function (canvas) {
 		new THREE.MeshBasicMaterial({map: new THREE.CanvasTexture(canvas)})
 	);
 	var pixels = 1/root.scale.x;
-	info.scale.set(450 * pixels, 600 * pixels, 1);
-	info.position.set(-5, -2, 1.5);
+	info.scale.set(400 * pixels, 500 * pixels, 1);
+	info.position.set(-5, -2, 1.7);
 	info.rotation.set(Math.PI / 2, Math.PI / 2, 0);
-	info.rotateOnAxis(new THREE.Vector3(1, 0, 0), -20 * Math.PI / 180);
+	info.rotateOnAxis(new THREE.Vector3(1, 0, 0), -10 * Math.PI / 180);
 	root.add(info);
 });
